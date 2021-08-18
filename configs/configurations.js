f6aaf07a4b478568988ceaf5523f64083eff8ae4
Nabin <81064628+dakshya-nabin@users.noreply.github.com>
2021-08-18 17:28:34 +0545

    Update configurations.js

--- configs/configurations.js
+++ configs/configurations.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongodbURL: `mongodb+srv://dakshya:${process.env.MONGO_DB_PASS}@cluster0.bis6o.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
+  mongodbURL: `mongodb+srv://dakshya:${process.env.MONGO_DB_PASS}@cluster0.bis6o.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
   PORT: process.env.PORT || 4040,
 };
