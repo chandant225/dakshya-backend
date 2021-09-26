269db66ac5258abf70bffa9a57276bc47c0378be
Mahesh Aryal <30760395+aryalmahesh775@users.noreply.github.com>
2021-09-26 15:23:09 +0545

    update coupon
    update coupon update coupon update coupon update coupon

--- routes/couponRoute.js
+++ routes/couponRoute.js
@@ -2,6 +2,7 @@ const express = require("express");
 const {
   add_coupon,
   redeem_coupon,
+  delete_coupon,
   get_coupons
 } = require("../controllers/couponController");
 const router = express.Router();
@@ -9,5 +10,6 @@ const router = express.Router();
 router.post("/coupon/add", add_coupon);
 router.post("/coupon/redeem", redeem_coupon);
 router.get("/coupons", get_coupons);
+router.post("/coupons/:coupon_id", delete_coupon)
 
 module.exports = router;
